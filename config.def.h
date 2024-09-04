5bf18a16e77c7d08482daebf86890840fbbb68c7
CharaD7 <jijakahn6@gmail.com>
2024-09-04 09:23:31 +0000

    Set fonts to Fira Code iScript

--- config.def.h
+++ config.def.h
@@ -5,8 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true" };
+/*static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";*/
+static char *font = "Fira Code iScript :pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = { "Fira Code iScript :pixelsize=15:antialias=true:autohint=true" };
+/*static char *font2[] = { "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true" };*/
 static int borderpx = 0;
 
 /*
